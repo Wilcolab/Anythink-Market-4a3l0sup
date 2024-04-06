d12885b599bd7d7513aecafa31b7c5a91cbe4c41
adriaanemd <adriaan@e-md.co.za>
2024-04-06 12:03:22 +0000

    grab an image using dall-e-2 init seeding sort of

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,13 +1,16 @@
-var mongoose = require("mongoose");
-var uniqueValidator = require("mongoose-unique-validator");
-var slug = require("slug");
-var User = mongoose.model("User");
+var mongoose = require("mongoose"),
+  uniqueValidator = require("mongoose-unique-validator"),
+  slug = require("slug"),
+  User = mongoose.model("User");
+var { OpenAI } = require("openai");
+
+const openai = new OpenAI();
 
 var ItemSchema = new mongoose.Schema(
   {
     slug: { type: String, lowercase: true, unique: true },
-    title: {type: String, required: [true, "can't be blank"]},
-    description: {type: String, required: [true, "can't be blank"]},
+    title: { type: String, required: [true, "can't be blank"] },
+    description: { type: String, required: [true, "can't be blank"] },
     image: String,
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
@@ -19,7 +22,7 @@ var ItemSchema = new mongoose.Schema(
 
 ItemSchema.plugin(uniqueValidator, { message: "is already taken" });
 
-ItemSchema.pre("validate", function(next) {
+ItemSchema.pre("validate", function (next) {
   if (!this.slug) {
     this.slugify();
   }
@@ -27,24 +30,47 @@ ItemSchema.pre("validate", function(next) {
   next();
 });
 
-ItemSchema.methods.slugify = function() {
+ItemSchema.methods.slugify = function () {
   this.slug =
     slug(this.title) +
     "-" +
     ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
 };
 
-ItemSchema.methods.updateFavoriteCount = function() {
+ItemSchema.pre("save", async function (next) {
+  if (!this.image) {
+    try {
+      await this.imagify();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  next();
+});
+
+// possibly utility functiuon
+ItemSchema.methods.imagify = async function () {
+  const response = await openai.images.generate({
+    model: "dall-e-2",
+    prompt: this.title + this.description,
+    n: 1,
+    size: "256x256",
+  });
+  console.log(`Resp: ${JSON.stringify(response)}`);
+  this.image = response.data[0].url;
+}
+
+ItemSchema.methods.updateFavoriteCount = function () {
   var item = this;
 
-  return User.count({ favorites: { $in: [item._id] } }).then(function(count) {
+  return User.count({ favorites: { $in: [item._id] } }).then(function (count) {
     item.favoritesCount = count;
 
     return item.save();
   });
 };
 
-ItemSchema.methods.toJSONFor = function(user) {
+ItemSchema.methods.toJSONFor = function (user) {
   return {
     slug: this.slug,
     title: this.title,
--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,56 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const faker = require('faker');
+
+// Load environment variables
+dotenv.config(); 
+
+// Import your models
+const User = require('./models/User.js');
+const Item = require('./models/Item.js');
+const Comment = require('./models/Comment.js');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB: ', err));
+
+// Seed data generation
+const generateUsers = (numUsers) => {
+  const users = [];
+  for (let i = 0; i < numUsers; i++) {
+    users.push({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      // ...other user properties
+    });
+  }
+  return users;
+};
+
+// Generate similar functions for items and comments:
+const generateItems = (numItems) => { /* ... */ };
+const generateComments = (numComments, users, items) => { /* ... */ };
+
+// Seed function
+const seedDB = async () => {
+  await User.deleteMany({}); // Clear the all collections first
+  await Item.deleteMany({});
+  await Comment.deleteMany({});
+
+  const users = generateUsers(100); // Generate 10 sample users
+  const createdUsers = await User.insertMany(users);
+
+  const items = generateItems(100); // Generate 5 sample items
+  const itemsWithUserRefs = items.map(item => ({ ...item, user: createdUsers[Math.floor(Math.random() * createdUsers.length)]._id}));
+  const createdItems = await Item.insertMany(itemsWithUserRefs);
+
+  const comments = generateComments(100, createdUsers, createdItems); 
+  await Comment.insertMany(comments);
+
+  console.log('Database seeded!');
+  mongoose.disconnect();
+};
+
+seedDB();
